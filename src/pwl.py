1df59aa0d5c3d6584fd9bc76dbd744576bbbeb9c
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2023-06-23 12:51:08 -0300

    WIP: fix some bugs, parallelize PWL over datasets

--- src/pwl.py
+++ src/pwl.py
@@ -1,3 +1,7 @@
+from typing import Union
+from multiprocessing import Pool
+from joblib import cpu_count
+
 import numpy as np
 import pandas as pd
 
@@ -12,6 +16,7 @@ class PWL():
 
     def fit(self, df: pd.DataFrame,):
         df_ = df.round(6)  # avoid errors when comparing floats
+        df_ = df_.sort_values(by=self.inputs[::-1])
 
         X = df_[self.inputs].values
         y = df_[self.output].values
@@ -22,56 +27,86 @@ class PWL():
 
         self.curve = df_.set_index(self.inputs)[self.output]
     
-    def __call__(self, x: np.array) -> float:
-        n = len(self.n_X)  # number of variables
+    def __call__(self, x: Union[np.array, pd.DataFrame], n_jobs=None) -> float:
+        if isinstance(x, pd.DataFrame):
+            df_ = x.round(6)  # avoid errors when comparing floats
 
-        # find the breakpoints around x, which form an hypercube, and the
-        # position of x within this hypercube (`eta`)
-        hypercube_limits, eta = self.get_hypercube_containing_x(x)
+            X = df_[self.inputs].values
+            # y = df_[self.output].values
 
-        # weight of each vertex of the hypercube.
-        # theta.shape == (2**n,2)  # this vector is not actually needed
+            if n_jobs == -1:
+                n_jobs = cpu_count()
+            elif n_jobs is None:
+                n_jobs = cpu_count() // 2
 
-        # indices of theta in crescent order, that is, theta_indices[3] == [0,1,1] means
-        # that theta[3] is associated with the vertex with x0_L, x1_H and x2_H
-        theta_indices = np.mgrid[*[slice(2) for _ in range(n)]].T.reshape(-1,n)[:,::-1]
+            n_jobs = int(n_jobs)
+            if n_jobs > 1:
+                y_hat = list(Pool(n_jobs).map(self, X))
+                y_hat = np.array(y_hat)
+            else:
+                y_hat = np.apply_along_axis(self, 1, X)
 
-        # constraints on "high" faces
-        A_H, b_H = self.get_face_constraints(eta, theta_indices, high=True)
+            return y_hat
+        else:
+            n = len(self.n_X)  # number of variables
 
-        # constraints on "low" faces
-        A_L, b_L = self.get_face_constraints(eta, theta_indices, high=False)
+            # find the breakpoints around x, which form an hypercube, and the
+            # position of x within this hypercube (`eta`)
+            hypercube_limits, eta = self.get_hypercube_containing_x(x)
+            if hypercube_limits is np.nan:
+                # this happens when the required point is outside the breakpoints
+                return np.nan
 
-        assert np.all(A_L + A_H == 1)
-        assert np.all(b_L + b_H == 1)
+            # weight of each vertex of the hypercube.
+            # theta.shape == (2**n,2)  # this vector is not actually needed
 
-        # constraints on the output value
-        A_y, b_y = self.get_output_constraints(hypercube_limits, theta_indices)
+            # indices of theta in crescent order, that is, theta_indices[3] == [0,1,1] means
+            # that theta[3] is associated with the vertex with x0_L, x1_H and x2_H
+            theta_indices = np.mgrid[*[slice(2) for _ in range(n)]].T.reshape(-1,n)[:,::-1]
 
-        # set up optimization
-        A = np.bmat([
-            [A_L,np.zeros((n,1))],
-            [A_H,np.zeros((n,1))],
-            [A_y,np.ones((1,1))],
-        ])
-        b = np.bmat([b_L, b_H, b_y]).T
+            # constraints on "high" faces
+            A_H, b_H = self.get_face_constraints(eta, theta_indices, high=True)
 
-        # maximize the y value
-        c = np.eye(len(theta_indices) + 1)[-1]
+            # constraints on "low" faces
+            A_L, b_L = self.get_face_constraints(eta, theta_indices, high=False)
 
-        # all thetas have to be limited to (0,1)
-        bounds = [(0.,1.),] * len(theta_indices)
-        bounds.append((0,None))
+            assert np.all(A_L + A_H == 1)
+            assert np.all(b_L + b_H == 1)
 
-        res = linprog(c, A_eq=A, b_eq=b, bounds=bounds)
+            # constraints on the output value
+            A_y, b_y = self.get_output_constraints(hypercube_limits, theta_indices)
 
-        return res.x[-1]
+            if A_y is np.nan:
+                return np.nan
+
+            # set up optimization
+            A = np.bmat([
+                [A_L,np.zeros((n,1))],
+                [A_H,np.zeros((n,1))],
+                [A_y,np.ones((1,1))],
+            ])
+            b = np.bmat([b_L, b_H, b_y]).T
+
+            # maximize the y value
+            c = np.eye(len(theta_indices) + 1)[-1]
+
+            # all thetas have to be limited to (0,1)
+            bounds = [(0.,1.),] * len(theta_indices)
+            bounds.append((0,None))
+
+            res = linprog(c, A_eq=A, b_eq=b, bounds=bounds)
+
+            return res.x[-1]
 
     def get_output_constraints(self, hypercube_limits, theta_indices):
         y_vertices = list()
-        for theta_index in theta_indices:
-            x_vertex = np.diag(hypercube_limits.T[theta_index])
-            y_vertices.append(self.curve.loc[*x_vertex])
+        try:
+            for theta_index in theta_indices:
+                x_vertex = np.diag(hypercube_limits.T[theta_index])
+                y_vertices.append(self.curve.loc[*x_vertex])
+        except KeyError:
+            # one of the vertices is infeasible
+            return np.nan, np.nan
 
         y_vertices = np.array(y_vertices)
 
@@ -102,8 +137,15 @@ class PWL():
             x_i_breakpoints = self.X_breakpoints[i]
             
             # faces of the hypercube of breakpoints that enclose the input
-            x_i_L = x_i_breakpoints[x_i_breakpoints <= x[i]][-1]
-            x_i_H = x_i_breakpoints[x_i_breakpoints > x[i]][0]
+            try:
+                x_i_L = x_i_breakpoints[x_i_breakpoints <= x[i]][-1]
+                x_i_H = x_i_breakpoints[x_i_breakpoints > x[i]][0]
+            except IndexError:
+                try:
+                    x_i_L = x_i_breakpoints[x_i_breakpoints < x[i]][-1]
+                    x_i_H = x_i_breakpoints[x_i_breakpoints >= x[i]][0]
+                except IndexError:
+                    return np.nan, np.nan
             hypercube_limits.append([x_i_L, x_i_H])
 
             # relative position of the input with respect to the hypercube's faces
